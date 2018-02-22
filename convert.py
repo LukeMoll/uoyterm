e55991ceef03af6de7e9f1f89f2b15503b6293a3
Luke Moll <luke.moll99@gmail.com>
2018-02-22 21:55:09 +0000

    Added timestamp to output data

--- convert.py
+++ convert.py
@@ -1,22 +1,45 @@
 #!/usr/bin/python3
 import os
-from yaml import load
+import yaml
+import json
+from datetime import date
+import time
 
+INPUT_DIR = "input"
+OUTPUT_DIR = "output"
 def main():
-    INPUT_DIR = "input"
     if os.path.exists(INPUT_DIR) and os.path.isdir(INPUT_DIR):
-        print("Folder", INPUT_DIR+"/", "found.")
-        input_files = [os.path.join(INPUT_DIR,fn) for fn in os.listdir(INPUT_DIR) if fn[-5:] == '.yaml']
-        print(len(input_files), "files found.")
-        parseYAML(input_files)
+        if os.path.exists(OUTPUT_DIR) and os.path.isdir(OUTPUT_DIR):
+            print("Folder", INPUT_DIR+"/", "found.")
+            input_files = [os.path.join(INPUT_DIR, fn) for fn in os.listdir(INPUT_DIR) if fn[-5:] == '.yaml']
+            print(len(input_files), "files found.")
+            data = parseYAML(input_files)
+            for yearData in data.values():
+                enrichYear(yearData)
+            writeJSON(OUTPUT_DIR, data)
+        else:
+            print("No output folder")
     else:
-        print("No input folder")    
+        print("No input folder")
 
 def parseYAML(input_files):
+    allData = {}
     for filename in input_files:
         stream = open(filename, 'r')
-        data = load(stream)
-        print(data)
+        data = yaml.load(stream)
+        allData[data['year']] = data
+    return allData
+
+def enrichYear(yearData):
+    for term in ["spr", "sum", "aut"]:
+        if term in yearData:
+            yearData[term]['start_ts'] = time.mktime(yearData[term]['start'].timetuple())
+            yearData[term]['end_ts']  =  time.mktime(yearData[term]['end'].timetuple())
+
+def writeJSON(outputDir, data):
+    for e in data.items():
+        with open(os.path.join(outputDir, str(e[0]) + '.json'), 'w+') as file:
+            file.write(json.dumps(e[1], default=str))
 
 if __name__ == '__main__':
     main()
